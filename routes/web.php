093962ab6fe25cee1fe9346554f3a3fd7c893acb
samuel <komangsamuel123@gmail.com>
2024-12-02 18:14:37 +0700

    Menambahkan rute CRUD untuk items di web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,12 @@
-<?php
-
-use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuestController;
 
-Route::get('/', function () {
-    return view('main');
+Route::controller(GuestController::class)->group(function() {
+Route::get('/', 'getHome')->name('home');
+Route::post('/guests', 'createGuest')->name('createGuest');
+Route::get('/edit-reservation/{guest_id}', 'getEditReservation')->name('getEditReservation');
+Route::post('/edit-reservation', 'editReservation')->name('editReservation');
+Route::post('/delete-reservation/{guest_id}', 'deleteReservation')->name('deleteReservation');
+Route::post('/update-guest/{guest_id}', 'updateGuest')->name('updateGuest');
+Route::post('/delete-guest/{guest_id}', 'deleteGuest')->name('deleteGuest');
 });
-
-Route::get('/item-list', [ItemController::class, 'fetchItemList']);
-Route::get('/add-item', [ItemController::class, 'showAddItemForm'])->name('showAddItemForm');
-Route::post('/add-item/submit', [ItemController::class, 'storeItem'])->name('storeItem');
