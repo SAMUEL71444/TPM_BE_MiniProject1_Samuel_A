bb10c0655542a9ad00cc9e92e73ea077d2f0c78c
samuel <komangsamuel123@gmail.com>
2024-12-02 21:07:45 +0700

    Menambahkan tombol edit dan delete ke halaman daftar items

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -9,10 +9,23 @@
 
 <body>
     <h1>Daftar Items</h1>
+    <a href="{{ route('items.create') }}">
+        <button type="button">Add Item</button>
+    </a>
     @if($items->count())
     <ul>
         @foreach($items as $item)
-        <li>{{ $item->name }} - {{ $item->description }}</li>
+        <li>
+            {{ $item->name }} - {{ $item->description }}
+            <a href="{{ route('items.edit', $item->id) }}">
+                <button type="button">Edit</button>
+            </a>
+            <form action="{{ route('items.destroy', $item->id) }}" method="POST" style="display:inline;">
+                @csrf
+                @method('DELETE')
+                <button type="submit" onclick="return confirm('Are you sure?')">Delete</button>
+            </form>
+        </li>
         @endforeach
     </ul>
     @else
